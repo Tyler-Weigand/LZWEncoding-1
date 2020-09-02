772326f757daeb7337de2ecaf69061d3eee45c86
nataisha <70167536+nataisha@users.noreply.github.com>
2020-09-02 13:12:29 -0700

    Update lzwEncoding.java

--- lzwEncoding.java
+++ lzwEncoding.java
@@ -1,1 +1,23 @@
 //submit the link to the github repo
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public class IZWEncoding{
+	publc main 
+	//an arraylist of every letter in the txt file
+	public ArrayList letArray;
+
+
+	//reading in the original file 
+	public ArayList letters (){
+	String file1 = "encodingfile.txt";
+	FileReader fr = new FileReader ("encodingfile.txt");
+	BufferedReader br = new BufferedReader (fr);
+	for (int i = 0; i < file1.length (); i++ ){
+		letArray.add (file1.toString (i, i+1)); 
+	}
+
+	}
+
+}
