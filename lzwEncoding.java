71baa342ca37d7765fe5161b0c70ec1b0dc69830
yvettejade <43455728+yvettejade@users.noreply.github.com>
2020-09-07 23:28:52 -0700

    LZW, Ascii, Ascii

--- lzwEncoding.java
+++ lzwEncoding.java
@@ -3,40 +3,62 @@ import java.util.*;
 import java.io.*;
 public class lzwEncoding 
 {
-	public static HashMap<String,Integer> init(HashMap<String,Integer> table)
+	public static HashMap<String,Character> init(HashMap<String,Character> table)
 	{
-		for(int a = 0; a < 126; a++)
+		for(int a = 0; a < 128; a++)
 		{
-			char current = (char)a;
-			table.put(current+"",a);
+			char current = (char)(a);
+			// (pattern (string) + corresponding ascii (char))
+			table.put(current+"",current);
 		}
 		return table;
 	}
 
 	public static void main(String[] args) throws IOException
 	{
-		HashMap<String, Integer> table = new HashMap<String, Integer>();
+		// the table containing the pattern and corresponding ascii - "a" -> 'a'
+		HashMap<String, Character> table = new HashMap<String, Character>();
+		// holds the encoded message
+		StringBuilder encoding = new StringBuilder("");
+
+		// fill the table with the standard ascii 1-128
 		init(table);
+
 		BufferedReader br = new BufferedReader(new FileReader("lzw.txt"));
 		int current = br.read();
-		String build = "";
-		build += (char)current;
-		String encoding = "";
-		int num = 26;
+
+		// last char of previous pattern
+		String prev = (char)current + "";
+
+		// the next available ascii
+		int num = 128;
+
 		while(current != -1)
 		{
 			current = br.read();
-			String temp = build + current;
-			System.out.println(table.containsKey(temp));
+			// current portion of the text we're dealing with 
+			String temp = prev + (char)current;
+
+			// new pattern!
 			if(!table.containsKey(temp))
 			{
-				encoding += table.get(build);
-				table.put(temp, num);
+				// encode previous
+				encoding.append(table.get(prev));
+
+				// max 256 bc the extended ascii table ends at 255, so we can't represent anything past 255
+				if(num < 256)
+					table.put(temp, (char)num);
+
 				num++;
-				build = "";
+				// reset bc we've already encoded the previous
+				prev = "";
 			}
-			build += current;
+			// add to or set the previous
+			prev += (char)current;
 		}
-		System.out.println(encoding);
+		br.close();
+		BufferedWriter bw = new BufferedWriter(new FileWriter("lzwEncoded.txt"));
+		bw.write(encoding.toString());
+		bw.close();
 	}
 }
