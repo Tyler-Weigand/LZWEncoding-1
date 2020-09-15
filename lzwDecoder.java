e06687f17cdb1f3a889a23b026d6d13098754771
QPC1234 <quinncallaghan@gmail.com>
2020-09-15 10:25:25 -0700

    what I have so far

--- lzwDecoder.java
+++ lzwDecoder.java
@@ -2,20 +2,35 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
 
 public class lzwDecoder {
 	public lzwDecoder() {}
 	
-	public void decode(String inputFileName) throws FileNotFoundException {
+	public void decode(String inputFileName) throws IOException {
 		BufferedReader br = new BufferedReader (new FileReader(inputFileName));
-		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		HashMap<Integer, String> encodingtable = new HashMap<Integer, String>();
 		PrintWriter pw = new PrintWriter(new File("output"));
+		int current = 0;
 		for(int a = 0; a < 128; a++) {
-			int current = (char)(a);
-			map.put(current + "", current);
+			current = a;
+			encodingtable.put(current, (char)current + "");
 		}
-		
+		int code; //current code
+		String encoding = ""; //Tracks the string of letters to add to the encoding table
+		while(br.ready()) {
+			code = br.read();
+			encoding+=(char)code + "";
+			if (!(encodingtable.containsValue(encoding))) {
+				encodingtable.put(current, encoding + "");
+				current++;
+				encoding = encoding.substring(encoding.length() - 1);
+			}
+			if (encodingtable.containsKey(code)) 
+				pw.print(encodingtable.get(code));
+			
+		}
 	}
 }
