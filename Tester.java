bd6aab40b5ebc191271771aa3a9c72b2bde384b3
QPC1234 <quinncallaghan@gmail.com>
2020-09-15 10:36:09 -0700

    fixed output device

--- Tester.java
+++ Tester.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Tester {
 	public static void main (String[] args) throws IOException {
 		lzwEncoding x = new lzwEncoding();
-		x.encode("big.txt", "output");
+		x.encode("big.txt", "encoded");
 		lzwDecoder y = new lzwDecoder();
 		y.decode("output");
 	}
