7ef2303f3003819cd1d65de16a1b262e97aa8690
QPC1234 <quinncallaghan@gmail.com>
2020-09-18 08:58:23 -0700

    build table

--- lzwDecoder.java
+++ lzwDecoder.java
@@ -22,19 +22,16 @@ public class lzwDecoder {
 		String encoding = ""; //Tracks the string of letters to add to the encoding table
 		while(br.ready()) {
 			code = br.read();
-			encoding+=(char)code + "";
-			if (!(encodingtable.containsValue(encoding))) {
-				encodingtable.put(current, encoding + "");
+			if (encodingtable.get(code) == null) {
+				encoding+=encoding.substring(0, 1);
+				encodingtable.put(current, encoding);
 				current++;
-				encoding = encoding.substring(encoding.length() - 1);
 			}
-			if (encodingtable.containsKey(code)) 
-				pw.print(encodingtable.get(code));
-			else {
-				encodingtable.put(current, encoding + encoding.substring(0, 1));
-				pw.print(encodingtable.get(current));
+			encoding+=encodingtable.get(code) + "";
+			if (!(encodingtable.containsValue(encoding))) {
+				encodingtable.put(current, encoding);
 				current++;
-				encoding = encoding.substring(encoding.length() - 1);
+				encoding = encoding.substring(encoding.length()-1);
 			}
 		}
 		pw.close();
