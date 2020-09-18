ade47b9fd6ce58d798e49e79017e5a69bff777e7
QPC1234 <quinncallaghan@gmail.com>
2020-09-18 09:20:39 -0700

    should work in theory

--- lzwDecoder.java
+++ lzwDecoder.java
@@ -11,28 +11,29 @@ public class lzwDecoder {
 	
 	public void decode(String inputFileName) throws IOException {
 		BufferedReader br = new BufferedReader (new FileReader(inputFileName));
-		HashMap<Integer, StringBuffer> encodingtable = new HashMap<Integer, StringBuffer>();
+		HashMap<Integer, String> encodingtable = new HashMap<Integer, String>();
 		PrintWriter pw = new PrintWriter(new File("output"));
 		int current = 0;
 		for(int a = 0; a < 128; a++) {
 			current = a;
-			encodingtable.put(current, new StringBuffer((char)current + ""));
+			encodingtable.put(current, ((char)current + ""));
 		}
 		int code; //current code
-		StringBuffer encoding = new StringBuffer(); //Tracks the string of letters to add to the encoding table
+		current++;
+		String encoding = ""; //Tracks the string of letters to add to the encoding table
 		while(br.ready()) {
 			code = br.read();
 			if (encodingtable.get(code) == null) {
-				encoding.append(encoding.substring(0, 1));
+				encoding+=(encoding.substring(0, 1));
 				encodingtable.put(current, encoding);
 				current++;
 			}
 			else
-				encoding.append(encodingtable.get(code) + "");
+				encoding+=(encodingtable.get(code));
 			if (!(encodingtable.containsValue(encoding))) {
 				encodingtable.put(current, encoding);
 				current++;
-				encoding = new StringBuffer(encoding.substring(encoding.length()-1));
+				encoding = (encoding.substring(encoding.length()-1));
 			}
 			pw.print(encodingtable.get(code));
 		}
