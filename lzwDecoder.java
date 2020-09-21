b2fc2bad06681f268b7c01a85b7b0c785144800b
QPC1234 <quinncallaghan@gmail.com>
2020-09-20 18:41:37 -0700

    More Commenting

--- lzwDecoder.java
+++ lzwDecoder.java
@@ -18,9 +18,9 @@ public class lzwDecoder {
 		
 		//First iteration, priming variables to loop
 		int nextValue = INITIAL_TABLE_SIZE;
-		int currentCode = br.read();
-		String previousStr = "";
-		String currentStr = "";
+		int currentCode = br.read(); //represents the most recent encoded char
+		String previousStr = ""; //represents the most recently printed set of chars
+		String currentStr = ""; //represents the set of chars to be printed & added to the hashmap
 		
 		//Puts first combination (the first two letters) into the hashmap
 		currentStr = (char) currentCode +"";
@@ -36,11 +36,11 @@ public class lzwDecoder {
 		
 		while(br.ready()) {
 			currentCode = br.read();
-			if (encodingTable.containsKey(currentCode)) { //if current code is already in the hashmap, add the next encoding to the table
+			if (encodingTable.containsKey(currentCode)) { //if current code is already in the hashmap, add previous characters + 1st letter of current to hashmap
 				currentStr = encodingTable.get(currentCode);
 				encodingTable.put(nextValue, previousStr + currentStr.substring(0,1));
 			} 
-			else { //otherwise, add the current string + its first letter
+			else { //otherwise, add the previous string + its first letter
 				currentStr = previousStr + previousStr.substring(0,1);
 				encodingTable.put(nextValue, currentStr);
 			}
